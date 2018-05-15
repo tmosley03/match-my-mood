d9b8c2a92cf6838380ddf3059a3c6e659cd9c5e5
Luis <salazarluis0119@gmail.com>
2018-05-15 18:27:05 -0500

    COFFEE

--- DataBaseForSQL/InstructionForSQL/schema.sql
+++ DataBaseForSQL/InstructionForSQL/schema.sql
@@ -1,6 +1,66 @@
+-- will drop MovieApp_db if it exist currently. 
+DROP DATABASE IF EXIST MovieApp_db
+-- creates the MovieApp_db database.
 CREATE DATABASE MovieApp_db;
 
-USE MovieApp_db;
 
-CREATE TABLE Movie 
+
+
+-- makes it so all of the following code will affect MovieApp_db.
+USE MovieApp_db;
+-- create table "Movie" within MovieApp_db. 
+CREATE TABLE Movie (
+
+
+
+
+-- name is for UserName. 
+-- makes a string column called "name" which cannot contain null. 
+name VARCHAR(30) NOT NULL, 
+
+
+-- makes a boolean column called "likes_movies?" which cannot contain null. [I'm guessing True of False? select yes or no kind of thing?]
+likes_movies BOOLEAN NOT NULL, 
+
+
+-- makes a sting column called "list_of_movies"
+list_of_movies VARCHAR(30),
+
+
+-- makes a numeric column called person_age.
+person_age INTEGER(10)
+);
+
+    --NO IDEA. 
+  -- Sets id as this table's primary key which means all data contained within it will be unique --
+  --PRIMARY KEY (id)
+--);
+
+
+
+INSERT INTO Movie  (name, likes_movies, list_of_movies, person_age)
+VALUES ("FIRST LAST", TRUE, "Movie1,Movie2,Movie3", 100);
+
+INSERT INTO Movie  (name, likes_movies, list_of_movies, person_age)
+VALUES ("FIRST LAST", TRUE, "Movie1,Movie2,Movie3", 100);
+
+INSERT INTO Movie  (name, likes_movies, list_of_movies, person_age)
+VALUES ("FIRST LAST", TRUE, "Movie1,Movie2,Movie3", 100);
+
+INSERT INTO Movie  (name, likes_movies, list_of_movies, person_age)
+VALUES ("FIRST LAST", TRUE, "Movie1,Movie2,Movie3", 100);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
