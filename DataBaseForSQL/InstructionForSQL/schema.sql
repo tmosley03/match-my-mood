e08075802b8f0efe334e603bcedfb7911cf5d034
Luis <salazarluis0119@gmail.com>
2018-05-15 18:33:41 -0500

    coldCoffee

--- DataBaseForSQL/InstructionForSQL/schema.sql
+++ DataBaseForSQL/InstructionForSQL/schema.sql
@@ -38,20 +38,6 @@ person_age INTEGER(10)
 
 
 
-INSERT INTO Movie  (name, likes_movies, list_of_movies, person_age)
-VALUES ("FIRST LAST", TRUE, "Movie1,Movie2,Movie3", 100);
-
-INSERT INTO Movie  (name, likes_movies, list_of_movies, person_age)
-VALUES ("FIRST LAST", TRUE, "Movie1,Movie2,Movie3", 100);
-
-INSERT INTO Movie  (name, likes_movies, list_of_movies, person_age)
-VALUES ("FIRST LAST", TRUE, "Movie1,Movie2,Movie3", 100);
-
-INSERT INTO Movie  (name, likes_movies, list_of_movies, person_age)
-VALUES ("FIRST LAST", TRUE, "Movie1,Movie2,Movie3", 100);
-
-
-
 
 
 
---
+++ DataBaseForSQL/InstructionForSQL/seeds.sql
@@ -0,0 +1,14 @@
+
+INSERT INTO Movie  (name, likes_movies, list_of_movies, person_age)
+VALUES ("FIRST LAST", TRUE, "Movie1,Movie2,Movie3", 100);
+
+INSERT INTO Movie  (name, likes_movies, list_of_movies, person_age)
+VALUES ("FIRST LAST", TRUE, "Movie1,Movie2,Movie3", 100);
+
+INSERT INTO Movie  (name, likes_movies, list_of_movies, person_age)
+VALUES ("FIRST LAST", TRUE, "Movie1,Movie2,Movie3", 100);
+
+INSERT INTO Movie  (name, likes_movies, list_of_movies, person_age)
+VALUES ("FIRST LAST", TRUE, "Movie1,Movie2,Movie3", 100);
+
+
