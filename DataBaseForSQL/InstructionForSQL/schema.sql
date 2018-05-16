e67eb857dea57c5d565c1acece53fc8413549e50
Luis <salazarluis0119@gmail.com>
2018-05-15 19:59:38 -0500

    done

--- DataBaseForSQL/InstructionForSQL/schema.sql
+++ DataBaseForSQL/InstructionForSQL/schema.sql
@@ -1,68 +1,7 @@
-
---4
--- Connect to MySQL server
--- mysql -uroot
-
---no clue on what the two above sentences do. 
-
-
--- will drop MovieApp_db if it exist currently. 
-DROP DATABASE IF EXIST MovieApp_db
--- creates the MovieApp_db database.
-CREATE DATABASE MovieApp_db;
-
-
-
-
--- makes it so all of the following code will affect MovieApp_db.
-USE MovieApp_db;
--- create table "Movie" within MovieApp_db. 
-CREATE TABLE Movie (
-
-
-
-
--- name is for UserName. 
--- makes a string column called "name" which cannot contain null. 
-name VARCHAR(30) NOT NULL, 
-
-
--- makes a boolean column called "likes_movies?" which cannot contain null. [I'm guessing True of False? select yes or no kind of thing?]
-likes_movies BOOLEAN NOT NULL, 
-
-
-
--- makes a sting column called "list_of_movies"
-list_of_movies VARCHAR(30),
-
-
-
--- makes a sting column called "list_of_movies"
-list_of_Language VARCHAR(40),
--- just added the language option. 
-
-
-
--- makes a numeric column called person_age.
-person_age INTEGER(10)
-);
-
-    --NO IDEA. 
-  -- Sets id as this table's primary key which means all data contained within it will be unique --
-  --PRIMARY KEY (id)
---);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+CREATE TABLE `matchmymood_db`.`user_data` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `User_Name` VARCHAR(45) NOT NULL,
+  `All_Time_Favorite_Movie` VARCHAR(45) NOT NULL,
+  `Age` INT(30) NOT NULL,
+  `Language_other_than_English` VARCHAR(45) NULL,
+  PRIMARY KEY (`id`));
--- DataBaseForSQL/InstructionForSQL/seeds.sql
+++ DataBaseForSQL/InstructionForSQL/seeds.sql
@@ -1,17 +1,5 @@
-
-INSERT INTO Movie  (name, likes_movies, list_of_movies, list_of_Language, person_age)
-VALUES ("FIRST LAST", TRUE, "Movie1,Movie2,Movie3", "Language1,Language2,Language3", 100);
-
-
-INSERT INTO Movie  (name, likes_movies, list_of_movies, list_of_Language, person_age)
-VALUES ("FIRST LAST", TRUE, "Movie1,Movie2,Movie3", "Language1,Language2,Language3", 100);
-
-
-INSERT INTO Movie  (name, likes_movies, list_of_movies, list_of_Language, person_age)
-VALUES ("FIRST LAST", TRUE, "Movie1,Movie2,Movie3", "Language1,Language2,Language3", 100);
-
-
-INSERT INTO Movie  (name, likes_movies, list_of_movies, list_of_Language, person_age)
-VALUES ("FIRST LAST", TRUE, "Movie1,Movie2,Movie3", "Language1,Language2,Language3", 100);
-
-
+INSERT INTO `matchmymood_db`.`user_data` (`id`, `User_Name`, `All_Time_Favorite_Movie`, `Age`, `Language_other_than_English`) VALUES ('1', 'TheKing888', 'The Lion King', '25', 'Spanish');
+INSERT INTO `matchmymood_db`.`user_data` (`id`, `User_Name`, `All_Time_Favorite_Movie`, `Age`, `Language_other_than_English`) VALUES ('2', 'Run35Run', 'Mulan', '30', 'French');
+INSERT INTO `matchmymood_db`.`user_data` (`id`, `User_Name`, `All_Time_Favorite_Movie`, `Age`) VALUES ('3', 'TheKite55', 'The Walking Dead', '90');
+INSERT INTO `matchmymood_db`.`user_data` (`id`, `User_Name`, `All_Time_Favorite_Movie`, `Age`) VALUES ('4', 'Car4343', 'Lost in Space', '23');
+INSERT INTO `matchmymood_db`.`user_data` (`id`, `User_Name`, `All_Time_Favorite_Movie`, `Age`, `Language_other_than_English`) VALUES ('5', 'Door75', 'Breaking Bad', '31', 'Spanish');
